1405270636928adc62736779bd0e09a34b2beea9
inikulin <ifaaan@gmail.com>
2016-06-29 17:47:23 +0300

    Implement testing with latest TestCafe API and data

--- test/test.js
+++ test/test.js
@@ -1,31 +1,7 @@
-var assert             = require('assert');
-var normalizeNewline   = require('normalize-newline');
-var read               = require('read-file-relative').readSync;
-var pluginTestingUtils = require('testcafe').pluginTestingUtils;
-var pluginFactory      = require('../lib');
-var testCalls          = require('./data/test-calls');
-
-function createReport (withColors) {
-    var outStream = {
-        data: '',
-
-        write: function (text) {
-            this.data += text;
-        }
-    };
-
-    var plugin = pluginTestingUtils.buildReporterPlugin(pluginFactory, outStream);
-
-    plugin.chalk.enabled = !plugin.noColors && withColors;
-    plugin.symbols       = { ok: '✓', err: '✖' };
-
-
-    testCalls.forEach(function (call) {
-        plugin[call.method].apply(plugin, call.args);
-    });
-
-    return outStream.data;
-}
+var assert           = require('assert');
+var normalizeNewline = require('normalize-newline');
+var read             = require('read-file-relative').readSync;
+var createReport     = require('./utils/create-report');
 
 it('Should produce report with colors', function () {
     var report   = createReport(true);
