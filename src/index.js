30284e0208b907fc24552609dd97fec2e2c9247a
inikulin <ifaaan@gmail.com>
2016-07-27 14:54:53 +0300

    Implement warnings. New error format. Bump version.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ export default function () {
             userAgents: null,
             passed:     0,
             total:      0,
-            fixtures:   []
+            fixtures:   [],
+            warnings:   []
         },
 
         reportTaskStart (startTime, userAgents, testCount) {
@@ -23,15 +24,23 @@ export default function () {
             this.report.fixtures.push(this.currentFixture);
         },
 
-        reportTestDone (name, errs, durationMs, unstable, screenshotPath) {
-            errs = errs.map(err => this.formatError(err));
+        reportTestDone (name, testRunInfo) {
+            var errs = testRunInfo.errs.map(err => this.formatError(err));
 
-            this.currentFixture.tests.push({ name, errs, durationMs, unstable, screenshotPath });
+            this.currentFixture.tests.push({
+                name,
+                errs,
+
+                durationMs:     testRunInfo.durationMs,
+                unstable:       testRunInfo.unstable,
+                screenshotPath: testRunInfo.screenshotPath
+            });
         },
 
-        reportTaskDone (endTime, passed) {
-            this.report.passed  = passed;
-            this.report.endTime = endTime;
+        reportTaskDone (endTime, passed, warnings) {
+            this.report.passed   = passed;
+            this.report.endTime  = endTime;
+            this.report.warnings = warnings;
 
             this.write(JSON.stringify(this.report, null, 2));
         }
